1012e44a0f68444c8de91ec00c9cbd57e9a08cea
SRINU42 <srinu.karnam01@gmail.com>
2025-02-01 12:04:05 +0530

    up

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ resource "null-resource" "test" {
         xyz = timestamp()
     }
     provisioner "local-exec" {
-        command = "echo Hello world - Env - ${var.env}"  
+        command =  "echo Hello world - Env - ${var.env}"  
     }
 }
