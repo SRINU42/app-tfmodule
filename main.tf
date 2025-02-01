2d55672249a06569cd2e8c6016c6ab416c24af0a
SRINU42 <srinu.karnam01@gmail.com>
2025-02-01 12:24:13 +0530

    q

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 
-resource "null-resource" "test" {
+resource "aws_instance" "web" {
     triggers = {
         xyz = timestamp()
     }
     provisioner "local-exec" {
         command =  "echo Hello world - Env - ${var.env}"  
     }
+
 }
