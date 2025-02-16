1a34c1f9248956409e4fed468da9275d0f671b87
SRINU42 <srinu.karnam01@gmail.com>
2025-02-16 19:55:17 +0530

    a

--- main.tf
+++ main.tf
@@ -82,10 +82,10 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = [aws_security_group.sg.id]
   iam_instance_profile   = aws_iam_instance_profile.instance_profile.name
 
-  tags = merge {{ 
+  tags = merge ({ 
     Name = "${var.component}-${var.env}" 
     } ,
-    var.tags } 
+    var.tags ) 
 }
 
 resource "aws_route53_record" "dns" {
